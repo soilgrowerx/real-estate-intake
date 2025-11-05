4c16c941a913ba1219f9bc2a9318e353e71ecbff
soilgrowerx <soilgrowerx@github.com>
2025-11-05 14:33:53 -0600

    fix: Replace service worker and fix paths

--- index.tsx
+++ index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 // Register the service worker for PWA functionality
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('service-worker.js')
       .then(registration => {
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       })
